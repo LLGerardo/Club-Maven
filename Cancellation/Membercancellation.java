5397552624d409070583cfde477ea6747e6bcc64
AroroBlue <65135755+AroroBlue@users.noreply.github.com>
2023-12-01 11:57:47 -0600

    Update Membercancellation.java

--- Cancellation/Membercancellation.java
+++ Cancellation/Membercancellation.java
@@ -1,5 +1,12 @@
 package Cancellation;
 //Arub
 public class Membercancellation {
-    
+    public void cancelMembership(Memeber member){
+        if(member.isActiveMember(){
+            member.setActiveMember(false);
+            System.out.println("the membership with this ID:" +  member.getMembershipId() + "had been canceled");
+        }else{
+            System.out.println("this member with this ID:" +  member.getMembershipId() + "is already canceled");
+        }
+    }
 }
