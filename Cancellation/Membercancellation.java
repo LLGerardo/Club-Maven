2033772b4836a33a8e23dc7957ea9ba9af3bad44
LLGerardo <lauralgerardo@gmail.com>
2023-12-02 13:13:57 -0600

    .

--- Cancellation/Membercancellation.java
+++ Cancellation/Membercancellation.java
@@ -1,14 +1,18 @@
 package Cancellation;
 //import java.util.Scanner;
+import Creation.Member;
+import Creation.MembershipCreation;
+import GUI.GUIPanel;
+import GUI.DatabaseConnector;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 //Arub
 public class Membercancellation {
-    public void cancelMembership(Memeber member){
+    public void cancelMembership(Member member){
         if(member.isActiveMember(){
             member.setActiveMember(false);
-            System.out.println("the membership with this ID:" +  member.getMembershipId() + "had been canceled");
+            System.out.println("the membership with this ID:" +  member.getMembershipId() + "has been canceled");
         }else{
             System.out.println("the membership with this ID:" +  member.getMembershipId() + "is already canceled");
         }
